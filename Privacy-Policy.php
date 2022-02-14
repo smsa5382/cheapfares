0ba59257a0d8ec36bc9c0db3d9be2050393d04cc
smsa5382 <98916736+smsa5382@users.noreply.github.com>
2022-02-14 16:09:17 +0500

    Add files via upload

---
+++ Privacy-Policy.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="​​Privacy Policy
+​">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Privacy Policy</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Privacy-Policy.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 4.4.0, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/logo1.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Privacy Policy">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body u-xl-mode"><header class="u-clearfix u-header u-header" id="sec-6ba8" data-image-width="1903" data-image-height="393"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="tel:+18886214991" class="u-active-none u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-btn-rectangle u-button-style u-hover-none u-none u-radius-0 u-text-body-color u-top-left-radius-0 u-top-right-radius-0 u-btn-1"><span class="u-icon"><svg class="u-svg-content" viewBox="0 0 405.333 405.333" x="0px" y="0px" style="width: 1em; height: 1em;"><path d="M373.333,266.88c-25.003,0-49.493-3.904-72.704-11.563c-11.328-3.904-24.192-0.896-31.637,6.699l-46.016,34.752    c-52.8-28.181-86.592-61.952-114.389-114.368l33.813-44.928c8.512-8.512,11.563-20.971,7.915-32.64    C142.592,81.472,138.667,56.96,138.667,32c0-17.643-14.357-32-32-32H32C14.357,0,0,14.357,0,32    c0,205.845,167.488,373.333,373.333,373.333c17.643,0,32-14.357,32-32V298.88C405.333,281.237,390.976,266.88,373.333,266.88z"></path></svg><img></span>&nbsp;+1 (888) 621-4991
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Reservation-Desk.html" style="padding: 10px 6px;">Reservation Desk</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Privacy-Policy.html" style="padding: 10px 6px;">Privacy Policy</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Terms-Conditions.html" style="padding: 10px 6px;">Terms Conditions</a>
+</li></ul>
+          </div>
+          <div class="u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Reservation-Desk.html">Reservation Desk</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Privacy-Policy.html">Privacy Policy</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Terms-Conditions.html">Terms Conditions</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+        <a href="https://nicepage.com" class="u-image u-logo u-image-1" data-image-width="439" data-image-height="61">
+          <img src="images/logo1.png" class="u-logo-image u-logo-image-1">
+        </a>
+      </div></header>
+    <section class="u-align-center u-clearfix u-section-1" id="sec-95c2">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-text u-text-default u-text-1"> Privacy Policy </h1>
+        <p class="u-text u-text-2"> This page informs you of our 
+policies regarding the collection, use and disclosure of Personal 
+Information when you use our Service.
+We will not use or share your information with anyone except as described in this Privacy Policy.
+We use your Personal 
+Information for providing and improving the Service. By using the 
+Service, you agree to the collection and use of information in 
+accordance with this policy. Unless otherwise defined in this Privacy 
+Policy, terms used in this Privacy Policy have the same meanings as in 
+our Terms and Conditions, accessible at our website
+Information Collection And Use
+While using our Service, we 
+may ask you to provide us with certain personally identifiable 
+information that can be used to contact or identify you. Personally 
+identifiable information (“Personal Information”) may include, but is 
+not limited to: <br> Name <br> Email address <br> Telephone number <br> Address <br> Log Data
+We collect information that 
+your browser sends whenever you visit our Service (“Log Data”). This Log
+ Data may include information such as your computer’s Internet Protocol 
+(“IP”) address, browser type, browser version, the pages of our Service 
+that you visit, the time and date of your visit, the time spent on those
+ pages and other statistics.
+Google AdSense &amp; DoubleClick Cookie
+Google, as a third party vendor, uses cookies to serve ads on our Service.
+Cookies
+Cookies are files with small 
+amount of data, which may include an anonymous unique identifier. 
+Cookies are sent to your browser from a web site and stored on your 
+computer’s hard drive.
+We use “cookies” to collect 
+information. You can instruct your browser to refuse all cookies or to 
+indicate when a cookie is being sent. However, if you do not accept 
+cookies, you may not be able to use some portions of our Service.
+Service Providers
+We may employ third party 
+companies and individuals to facilitate our Service, to provide the 
+Service on our behalf, to perform Service-related services or to assist 
+us in analyzing how our Service is used.
+These third parties have 
+access to your Personal Information only to perform these tasks on our 
+behalf and are obligated not to disclose or use it for any other 
+purpose.
+Security
+The security of your Personal 
+Information is important to us, but remember that no method of 
+transmission over the Internet, or method of electronic storage is 100% 
+secure. While we strive to use commercially acceptable means to protect 
+your Personal Information, we cannot guarantee its absolute security.
+Links To Other Sites
+Our Service may contain links 
+to other sites that are not operated by us. If you click on a third 
+party link, you will be directed to that third party’s site. We strongly
+ advise you to review the Privacy Policy of every site you visit.
+We have no control over, and 
+assume no responsibility for the content, privacy policies or practices 
+of any third party sites or services.
+Children’s Privacy
+Our Service does not address anyone under the age of 18 (“Children”).
+We do not knowingly collect 
+personally identifiable information from children under 18. If you are a
+ parent or guardian and you are aware that your child has provided us 
+with Personal Information, please contact us. If we discover that a 
+child under 18 has provided us with Personal Information, we will delete
+ such information from our servers immediately.
+Compliance With Laws
+We will disclose your Personal Information where required to do so by law or subpoena.
+Changes To This Privacy Policy
+We may update our Privacy 
+Policy from time to time. We will notify you of any changes by posting 
+the new Privacy Policy on this page.
+You are advised to review this
+ Privacy Policy periodically for any changes. Changes to this Privacy 
+Policy are effective when they are posted on this page.
+Contact Us
+If you have any questions about this Privacy Policy, please contact us at admin@budgetfares.com<br>
+        </p>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-c3bc"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1"> © 2022. 596 Hart Street, Miami FL 33131</p>
+        <p class="u-small-text u-text u-text-variant u-text-2"> We
+ do not guarantee the accuracy of any content, information, product, or 
+service that has been published on its website. Note that any such 
+description or information, which includes but is not limited to price, 
+infographics, graphics, photo, product or service description, etc., can
+ consist of inaccuracies and errors for which we will not be held 
+liable. Moreover, we reserve the right to edit, modify or change any 
+piece of information at any given time. In case a customer has made 
+reservation from this website under the published price that is wrong, 
+then customer will be given a chance to either continue the same 
+reservation under the correct price or to cancel the reservation without
+ any termination fee.</p>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse">
+            <a class="u-button-style u-nav-link" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-7917"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="svg-7917" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-nav-container">
+            <ul class="u-nav u-unstyled"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Reservation-Desk.html">Reservation Desk</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Privacy-Policy.html">Privacy Policy</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Terms-Conditions.html">Terms Conditions</a>
+</li></ul>
+          </div>
+          <div class="u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Reservation-Desk.html">Reservation Desk</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Privacy-Policy.html">Privacy Policy</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Terms-Conditions.html">Terms Conditions</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  </body>
+</html>
---
+++ Terms-Conditions.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="​Terms and Conditions">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Terms Conditions</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Terms-Conditions.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 4.4.0, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/logo1.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Terms Conditions">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body u-xl-mode"><header class="u-clearfix u-header u-header" id="sec-6ba8" data-image-width="1903" data-image-height="393"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="tel:+18886214991" class="u-active-none u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-btn-rectangle u-button-style u-hover-none u-none u-radius-0 u-text-body-color u-top-left-radius-0 u-top-right-radius-0 u-btn-1"><span class="u-icon"><svg class="u-svg-content" viewBox="0 0 405.333 405.333" x="0px" y="0px" style="width: 1em; height: 1em;"><path d="M373.333,266.88c-25.003,0-49.493-3.904-72.704-11.563c-11.328-3.904-24.192-0.896-31.637,6.699l-46.016,34.752    c-52.8-28.181-86.592-61.952-114.389-114.368l33.813-44.928c8.512-8.512,11.563-20.971,7.915-32.64    C142.592,81.472,138.667,56.96,138.667,32c0-17.643-14.357-32-32-32H32C14.357,0,0,14.357,0,32    c0,205.845,167.488,373.333,373.333,373.333c17.643,0,32-14.357,32-32V298.88C405.333,281.237,390.976,266.88,373.333,266.88z"></path></svg><img></span>&nbsp;+1 (888) 621-4991
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Reservation-Desk.html" style="padding: 10px 6px;">Reservation Desk</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Privacy-Policy.html" style="padding: 10px 6px;">Privacy Policy</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Terms-Conditions.html" style="padding: 10px 6px;">Terms Conditions</a>
+</li></ul>
+          </div>
+          <div class="u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Reservation-Desk.html">Reservation Desk</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Privacy-Policy.html">Privacy Policy</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Terms-Conditions.html">Terms Conditions</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+        <a href="https://nicepage.com" class="u-image u-logo u-image-1" data-image-width="439" data-image-height="61">
+          <img src="images/logo1.png" class="u-logo-image u-logo-image-1">
+        </a>
+      </div></header>
+    <section class="u-align-center u-clearfix u-section-1" id="sec-357e">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <h1 class="u-text u-text-default u-text-1"> Terms and Conditions</h1>
+        <p class="u-text u-text-2"> Please read these Terms of Use
+ (“Terms”, “Terms of Use”) carefully before using the website (the 
+“Service”) operated by (“us”, “we”, or “our”).
+Your access to and use of the 
+Service is conditioned on your acceptance of and compliance with these 
+Terms. These Terms apply to all visitors, users and others who access or
+ use the Service.
+By accessing or using the 
+Service you agree to be bound by these Terms. If you disagree with any 
+part of the terms then you may not access the Service.
+Accounts
+When you create an account 
+with us, you must provide us information that is accurate, complete, and
+ current at all times. Failure to do so constitutes a breach of the 
+Terms, which may result in immediate termination of your account on our 
+Service.
+You are responsible for 
+safeguarding the password that you use to access the Service and for any
+ activities or actions under your password, whether your password is 
+with our Service or a third-party service.
+You agree not to disclose your
+ password to any third party. You must notify us immediately upon 
+becoming aware of any breach of security or unauthorized use of your 
+account.
+Links To Other Web Sites
+Our Service may contain links to third-party web sites or services that are not owned or controlled by us.
+This website has no control 
+over, and assumes no responsibility for, the content, privacy policies, 
+or practices of any third party web sites or services. You further 
+acknowledge and agree that we shall not be responsible or liable, 
+directly or indirectly, for any damage or loss caused or alleged to be 
+caused by or in connection with use of or reliance on any such content, 
+goods or services available on or through any such web sites or 
+services.
+We strongly advise you to read
+ the terms and conditions and privacy policies of any third-party web 
+sites or services that you visit.
+Termination
+We may terminate or suspend 
+your account immediately, without prior notice or liability, for any 
+reason whatsoever, including without limitation if you breach the Terms.
+Upon termination, your right 
+to use the Service will immediately cease. If you wish to terminate your
+ account, you may simply discontinue using the Service.
+All provisions of the Terms 
+which by their nature should survive termination shall survive 
+termination, including, without limitation, ownership provisions, 
+warranty disclaimers, indemnity and limitations of liability.
+Disclaimer
+Your use of the Service is at 
+your sole risk. The Service is provided on an “AS IS” and “AS AVAILABLE”
+ basis. The Service is provided without warranties of any kind, whether 
+express or implied, including, but not limited to, implied warranties of
+ merchantability, fitness for a particular purpose, non-infringement or 
+course of performance.
+&nbsp;
+Changes
+&nbsp;
+We reserve the right, at our 
+sole discretion, to modify or replace these Terms at any time. If a 
+revision is material we will try to provide at least 30 days notice 
+prior to any new terms taking effect. What constitutes a material change
+ will be determined at our sole discretion.
+By continuing to access or use
+ our Service after those revisions become effective, you agree to be 
+bound by the revised terms. If you do not agree to the new terms, please
+ stop using the Service.
+Contact Us
+If you have any questions about these Terms, please contact us at admin@budgetfares.com</p>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-c3bc"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1"> © 2022. 596 Hart Street, Miami FL 33131</p>
+        <p class="u-small-text u-text u-text-variant u-text-2"> We
+ do not guarantee the accuracy of any content, information, product, or 
+service that has been published on its website. Note that any such 
+description or information, which includes but is not limited to price, 
+infographics, graphics, photo, product or service description, etc., can
+ consist of inaccuracies and errors for which we will not be held 
+liable. Moreover, we reserve the right to edit, modify or change any 
+piece of information at any given time. In case a customer has made 
+reservation from this website under the published price that is wrong, 
+then customer will be given a chance to either continue the same 
+reservation under the correct price or to cancel the reservation without
+ any termination fee.</p>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse">
+            <a class="u-button-style u-nav-link" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-7917"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="svg-7917" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-nav-container">
+            <ul class="u-nav u-unstyled"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Reservation-Desk.html">Reservation Desk</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Privacy-Policy.html">Privacy Policy</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Terms-Conditions.html">Terms Conditions</a>
+</li></ul>
+          </div>
+          <div class="u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Reservation-Desk.html">Reservation Desk</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Privacy-Policy.html">Privacy Policy</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Terms-Conditions.html">Terms Conditions</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  </body>
+</html>
